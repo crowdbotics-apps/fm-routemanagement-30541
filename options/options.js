2b91e75c57fcf96cda7422894299fe520a1f39ab
Crowdbotics <team@crowdbotics.com>
2021-09-14 17:26:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fm_routemanagement_30541Identifier",
+  url: "https://fm_routemanagement_30541Identifier.botics.co",
+  api: "https://fm_routemanagement_30541Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
